83cce48aefb6b2bd2a8a29ad7044ad331c963196
Godwin Obiomah <godwinobiomah@gmail.com>
2025-11-30 01:02:38 -0800

    Fix class-based view, URLs, and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library  # ✅ Must be exactly this line
-from .models import Book
+from django.views.generic.detail import DetailView
+from .models import Library, Book
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
