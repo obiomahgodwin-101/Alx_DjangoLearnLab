a81c7b864370cc6f567cdd0a93ddca3d8123f22f
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-03 08:59:35 -0800

    Update Book model and admin for Week 11

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from .models import Book
 
-@admin.register(Book)
+# ----------------------------------
+#  Book Admin
+# ----------------------------------
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    search_fields = ('title', 'author')
-    list_filter = ('publication_year',)
+    list_display = ['title', 'author', 'publication_year']
+    list_filter = ['publication_year']
+
+admin.site.register(Book, BookAdmin)
 
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+# ----------------------------------
+#  Book Model
+# ----------------------------------
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    author = models.CharField(max_length=200)
+    publication_year = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.title
 
