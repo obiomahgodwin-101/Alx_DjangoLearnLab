1622f68697466ea9108f420c41dd1397a1f25ceb
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-03 19:08:58 -0800

    Fixed custom user model setup, cleaned migrations, updated settings

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# ----------------------------------
-#  Book Admin
-# ----------------------------------
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info', {
+            'fields': ('date_of_birth', 'profile_photo'),
+        }),
+    )
+
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'publication_year']
-    list_filter = ['publication_year']
-
-admin.site.register(Book, BookAdmin)
-
+    list_display = ('title', 'author', 'publication_year')
+    list_filter = ('publication_year',)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,24 +1,2 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser
-from django.utils.translation import gettext_lazy as _
-
-@admin.register(CustomUser)
-class CustomUserAdmin(UserAdmin):
-    # Use the fields that exist in your CustomUser
-    fieldsets = (
-        (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name', 'date_of_birth', 'profile_photo')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2'),
-        }),
-    )
-    list_display = ('email', 'first_name', 'last_name', 'is_staff', 'is_superuser')
-    search_fields = ('email', 'first_name', 'last_name')
-    ordering = ['email']  # ✅ Updated ordering field to a valid field in CustomUser
-
+# No CustomUser imported here
