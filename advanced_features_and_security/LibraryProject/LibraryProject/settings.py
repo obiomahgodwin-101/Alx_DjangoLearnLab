493e3f17bf0111c554bdb3c3acd505fe850bc6cd
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-03 20:57:31 -0800

    Add SECURE_PROXY_SSL_HEADER for HTTPS behind proxy

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,26 +1,28 @@
 # SECURITY SETTINGS FOR HTTPS AND SECURE COOKIES
 
-# Redirect all HTTP requests to HTTPS
-SECURE_SSL_REDIRECT = True  # Ensures all requests are served via HTTPS
-
-# HTTP Strict Transport Security (HSTS)
-SECURE_HSTS_SECONDS = 31536000  # 1 year in seconds
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True  # Apply HSTS to all subdomains
-SECURE_HSTS_PRELOAD = True  # Allow domain to be preloaded in browsers
-
-# Secure cookies
-SESSION_COOKIE_SECURE = True  # Only send session cookies over HTTPS
-CSRF_COOKIE_SECURE = True     # Only send CSRF cookies over HTTPS
-
-# Additional security headers
-X_FRAME_OPTIONS = "DENY"             # Prevent clickjacking
-SECURE_CONTENT_TYPE_NOSNIFF = True   # Prevent MIME-type sniffing
-SECURE_BROWSER_XSS_FILTER = True     # Enable browser XSS filtering
-
-# DEBUG should be False in production
 DEBUG = False
 
-# Existing settings (don't remove)
+# Redirect all HTTP requests to HTTPS
+SECURE_SSL_REDIRECT = True
+
+# HTTP Strict Transport Security (HSTS)
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+# Secure cookies
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Additional security headers
+X_FRAME_OPTIONS = "DENY"
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_BROWSER_XSS_FILTER = True
+
+# Trust the proxy's HTTPS header
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Existing settings
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'bookshelf.CustomUser'
