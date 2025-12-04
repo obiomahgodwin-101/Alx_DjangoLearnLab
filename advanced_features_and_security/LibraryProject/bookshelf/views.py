4d63970a2867ece2fff9780ec269911c1b389c28
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-03 20:43:05 -0800

    Update views.py to import ExampleForm and add example_form_view

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,10 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
 from .models import Book
+from .forms import ExampleForm  # ✅ Import ExampleForm as required
 
-# This view must be named exactly "book_list" and use the permission_required decorator
+# ------------------------------
+# Book list view
+# ------------------------------
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'bookshelf/book_list.html', {'books': books})
+
+# ------------------------------
+# Example form view
+# ------------------------------
+@permission_required('bookshelf.can_create', raise_exception=True)
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+    return render(request, 'bookshelf/form_example.html', {'form': form})
 
