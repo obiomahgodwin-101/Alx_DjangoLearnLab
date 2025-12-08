5dd4d97a758dbebd501e7dadd7559a24f7439541
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-07 20:23:42 -0800

    Remove duplicate nested API folder from repository

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import AuthorListCreate, BookListCreate
-
-urlpatterns = [
-    path('authors/', AuthorListCreate.as_view()),
-    path('books/', BookListCreate.as_view()),
-]
-
