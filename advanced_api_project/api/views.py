6d58469e315ab942286573e1b5f6ccc08c51041d
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-07 21:55:14 -0800

    Week 13: Clean advanced_api_project setup with Author and Book models

---
+++ advanced_api_project/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import generics
+from .models import Author, Book
+from .serializers import AuthorSerializer, BookSerializer
+
+class AuthorListCreate(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+class BookListCreate(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
