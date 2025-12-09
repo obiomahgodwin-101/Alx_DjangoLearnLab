311e79493beecc35d78ba00d63f9de5a177f065c
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-08 16:34:53 -0800

    Fix: correct settings.py and project structure for advanced API project

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('authors/', views.AuthorListCreateView.as_view()),
-    path('books/', views.BookListCreateView.as_view()),
-]
-
