92d1d9fd1d2a2c201b622f7d7a9c34cb15b966cf
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-14 10:49:27 -0800

    Add DRF filtering, search, and ordering with django_filters

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.filters import SearchFilter, OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 from .models import Book
 from .serializers import BookSerializer
@@ -15,7 +15,7 @@ class BookListView(generics.ListAPIView):
 
     # Enable filtering, searching, and ordering
     filter_backends = [
-        DjangoFilterBackend,
+        rest_framework.DjangoFilterBackend,
         SearchFilter,
         OrderingFilter
     ]
