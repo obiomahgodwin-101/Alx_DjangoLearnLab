2cb46f32609e76496ee0e620fae0ccd3dc281f92
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-19 10:18:36 -0800

    Fix checker-required like/unlike views and URL patterns

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -21,10 +21,7 @@ class LikePostView(generics.GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(
-            user=request.user,
-            post=post
-        )
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
             return Response(
