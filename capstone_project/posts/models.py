f887f9080b5d4fa116beafa45178fc0ac9eff689
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-22 18:52:49 -0800

    Capstone Part 4: add Like model and relationships

--- capstone_project/posts/models.py
+++ capstone_project/posts/models.py
@@ -15,3 +15,23 @@ class Post(models.Model):
     def __str__(self):
         return f"Post by {self.author.username}"
 
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")
+
+    def __str__(self):
+        return f"{self.user.username} liked post {self.post.id}"
+
