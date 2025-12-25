fc2c876cf23a7e3b517b4643f896dfbc53a6ce56
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-24 20:06:26 -0800

    Capstone Part 4: add Notification API with auth protection

--- capstone_project/notifications/views.py
+++ capstone_project/notifications/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Notification
+from .serializers import NotificationSerializer
 
-# Create your views here.
+
+class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(
+            recipient=self.request.user
+        ).order_by("-created_at")
+
