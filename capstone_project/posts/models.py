e0df62a29e392afd277eb2f4f0000dfc4c58a272
Godwin Obiomah <godwinobiomah@gmail.com>
2025-12-25 20:35:10 -0800

    Capstone Part 4: fix Like model conflict and remove social_posts

--- capstone_project/posts/models.py
+++ capstone_project/posts/models.py
@@ -20,12 +20,12 @@ class Like(models.Model):
     user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="likes"
+        related_name="post_likes"
     )
     post = models.ForeignKey(
         Post,
         on_delete=models.CASCADE,
-        related_name="likes"
+        related_name="post_likes"
     )
     created_at = models.DateTimeField(auto_now_add=True)
 
@@ -33,5 +33,5 @@ class Like(models.Model):
         unique_together = ("user", "post")
 
     def __str__(self):
-        return f"{self.user.username} liked Post {self.post.id}"
+        return f"{self.user.username} liked post {self.post.id}"
 
